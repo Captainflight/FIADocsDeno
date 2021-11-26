7bb8655fc1992037108c2035d09550d5b18d1ea4
Captainflight <69525865+Captainflight@users.noreply.github.com>
2021-11-26 10:40:44 -0500

    Update config.ts

--- functions/config.ts
+++ functions/config.ts
@@ -7,7 +7,7 @@ type config = {
 
 // Little wrapper for reading config.json
 class Config {
-  #config: config = { webhooks: [] };
+  #config: config = { webhooks: [https://discord.com/api/webhooks/913814492742316094/K_3LvWwRPw-RA1q8jCBw1Eedp1cTMnz6Ka0BmlDVWQnG548BTFOrB18AUdTY6SrLIZA_] };
   constructor() {
     const rawFile = Deno.readFileSync(configPath);
     this.#config = JSON.parse(new TextDecoder().decode(rawFile));
